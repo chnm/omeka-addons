34b34d2f92c62777de3dbdc01b7960cc7ac99eaf
Patrick Murray-John <patrickmjchnm@gmail.com>
2011-07-25 15:32:01 -0400

    Removes class='post' on addon divs
    Thus, makes zerocrates happy.

--- single-omeka_theme.php
+++ single-omeka_theme.php
@@ -2,7 +2,7 @@
 
 	<div id="primary">
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<div class="post" id="post-<?php the_ID(); ?>">
+		<div id="post-<?php the_ID(); ?>">
 			<h1><?php the_title(); ?></h1>
 		<div class="omeka-addon">
 		<?php the_content(); ?>
