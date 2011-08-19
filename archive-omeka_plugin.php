c30d561c17c3fa1f695286e62f12bab795db6f29
Patrick Murray-John <patrickmjchnm@gmail.com>
2011-08-19 11:43:29 -0400

    Fixes #39 Fixes #40 Fixes #41
    le boom.

--- archive-omeka_plugin.php
+++ archive-omeka_plugin.php
@@ -7,6 +7,13 @@
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		
 			<div class="omeka-addon" id="post-<?php the_ID(); ?>">
+			<div class="omeka-addons-download-block">
+                <p class='omeka-addons-latest-release'>
+                	<a class='omeka-addons-button' href='<?php echo $releaseData["zip_url"]; ?>'>Download Latest</a>
+                </p>
+    			<?php $license = isset($releaseData['ini_data']['license']) ? $releaseData['ini_data']['license'] : 'unknown'; ?>
+                <p class='omeka-addons-license'><span>License</span>: <?php echo $license; ?></p>
+            </div>
 				<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
         				
         		<div class="omeka-addons-cats">
@@ -24,11 +31,7 @@
         		</div>
         		<?php $releaseData = omeka_addons_get_latest_release_data($post->ID); ?>
 		        <p class='omeka-addons-description'><?php echo $releaseData['ini_data']['description']; ?></p>
-                <?php $license = isset($releaseData['ini_data']['license']) ? $releaseData['ini_data']['license'] : 'unknown'; ?>
-                <p class='omeka-addons-license'><span>License</span>: <?php echo $license; ?></p>
-                <p class='omeka-addons-latest-release'>
-                	<a class='omeka-addons-button' href='<?php echo $releaseData["zip_url"]; ?>'>Download Latest: Ver. <?php echo $releaseData['ini_data']['version']; ?></a>
-                </p>
+
 			</div>
 		<?php endwhile; endif; ?>
 
--- taxonomy.php
+++ taxonomy.php
@@ -1,12 +1,19 @@
 <?php get_header(); ?>
 
-	<div id="primary">
+	<div id="primary" class="omeka-addons-taxonomy">
         <?php $term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) ); ?>
 		<h1>Plugins for <?php echo $term->name; ?></h1>
 
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		
-			<div class="post" id="post-<?php the_ID(); ?>">
+		<?php $releaseData = omeka_addons_get_latest_release_data($post->ID); ?>
+			<div id="post-<?php the_ID(); ?>">
+			<div class="omeka-addons-download-block">
+                <p class='omeka-addons-latest-release'>
+                	<a class='omeka-addons-button' href='<?php echo $releaseData["zip_url"]; ?>'>Download Latest</a>
+                </p>
+    			<?php $license = isset($releaseData['ini_data']['license']) ? $releaseData['ini_data']['license'] : 'unknown'; ?>
+                <p class='omeka-addons-license'><span>License</span>: <?php echo $license; ?></p>
+            </div>
 				<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
         				
         		<div class="omeka-addons-cats">
@@ -23,9 +30,8 @@
         		?>
         		</div>
 		        <?php the_content(); ?>
-
 			</div>
-			
+
 		<?php endwhile; endif; ?>
 	</div>
 
