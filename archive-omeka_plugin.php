ade080ba72af5fb020f1f2fa816111a9abc4a39d
Patrick Murray-John <patrickmjchnm@gmail.com>
2011-08-22 11:25:31 -0400

    Fixes problem with plugin archive page. Improves add-ons css

--- archive-omeka_plugin.php
+++ archive-omeka_plugin.php
@@ -5,7 +5,7 @@
 <p>The following plugins may be downloaded separately and installed to work with <strong>Omeka 1.0 or higher</strong>. Helpful instructions for installing plugins are on the <a href="http://omeka.org/codex/Managing_Plugins">codex</a>. If you are looking for plugins compatible with earlier versions of Omeka, see the list of <a href="/add-ons/plugins/010-plugins/">plugins compatible with Omeka 0.10</a> and <a href="/add-ons/plugins/09-plugins/">plugins compatible with Omeka 0.9</a>.</p>
 		<?php query_posts($query_string . '&orderby=title&order=ASC&posts_per_page=-1'); ?>
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		
+        <?php $releaseData = omeka_addons_get_latest_release_data($post->ID); ?>
 			<div class="omeka-addon" id="post-<?php the_ID(); ?>">
 			<div class="omeka-addons-download-block">
                 <p class='omeka-addons-latest-release'>
@@ -16,7 +16,6 @@
             </div>
 				<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
 
-        		<?php $releaseData = omeka_addons_get_latest_release_data($post->ID); ?>
 		        <p class='omeka-addons-description'><?php echo $releaseData['ini_data']['description']; ?></p>
         				
         		<div class="omeka-addons-cats">
